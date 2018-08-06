409117bce11c2c2504d11cbb38433490b009beff
Amr Elzawawy <amr22.elzawawy@ymail.com>
2018-08-06 12:27:24 +0200

    Added HomeActivity , Added to (values folders-build.gradle)-->WATCH FOR CONFLICT.

--- app/src/main/java/com/xcodesystemsinterns/studentmanager/HomeActivity.java
+++ app/src/main/java/com/xcodesystemsinterns/studentmanager/HomeActivity.java
@@ -1,13 +1,21 @@
 package com.xcodesystemsinterns.studentmanager;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
-
+    DataBaseHelper dataBaseHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        dataBaseHelper = new DataBaseHelper(this);
+        //boolean result = dataBaseHelper.removeClass(3);
+        //Cursor cursor =dataBaseHelper.getClassList();
+        //Log.d("myTAG",String.valueOf(cursor.getCount()));
+        //Log.d("myTAG",String.valueOf(result));
     }
 }
